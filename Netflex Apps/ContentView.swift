b78863dfbeca8c23aa700a4d36bbedecc33f5d20
Badar Maulana <45702477+badadarr@users.noreply.github.com>
2020-12-16 13:38:31 +0700

    Look it

---
+++ Netflex Apps/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Netflex Apps
+//
+//  Created by badar on 16/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Netflex Apps/Netflex_AppsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Netflex_AppsApp.swift
+//  Netflex Apps
+//
+//  Created by badar on 16/12/20.
+//
+
+import SwiftUI
+
+@main
+struct Netflex_AppsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
